cd01fb0f897431ad4e6e6edaafefc73eb93c58f1
Nguyen Tan Dung - K11 FUG HN <dungntse04679@fpt.edu.vn>
2019-11-29 21:35:01 +0700

    First

---
+++ Movies App/Movies App/API/NowPlayingAPI.swift
@@ -0,0 +1,9 @@
+//
+//  NowPlayingAPI.swift
+//  Movies App
+//
+//  Created by Nguyen Tan Dung on 11/29/19.
+//  Copyright © 2019 Nguyen Tan Dung. All rights reserved.
+//
+
+import Foundation
---
+++ Movies App/Movies App/NowPlayingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NowPlayingViewController.swift
+//  Movies App
+//
+//  Created by Nguyen Tan Dung on 11/29/19.
+//  Copyright © 2019 Nguyen Tan Dung. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
