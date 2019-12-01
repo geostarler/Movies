3959a18fb6bd349c7bfb2b2eaf2eaffab9decf88
Nguyen Tan Dung - K11 FUG HN <dungntse04679@fpt.edu.vn>
2019-12-02 04:18:06 +0700

    Show all data on screen

---
+++ Movies App/Movies App/View Controller/DetailMoviesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailMoviesViewController.swift
+//  Movies App
+//
+//  Created by Nguyen Tan Dung on 12/1/19.
+//  Copyright © 2019 Nguyen Tan Dung. All rights reserved.
+//
+
+import UIKit
+
+class DetailMoviesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Movies App/Movies App/View/TopRateCell.swift
@@ -0,0 +1,24 @@
+//
+//  TopRateCell.swift
+//  Movies App
+//
+//  Created by Nguyen Tan Dung on 12/2/19.
+//  Copyright © 2019 Nguyen Tan Dung. All rights reserved.
+//
+
+import UIKit
+
+class TopRateCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
